37ba4cae30f9305f2a8dbe1340f1b88ec45995d1
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2015-12-08 12:25:32 -0200

    client sending more than one message

--- client.py
+++ client.py
@@ -5,8 +5,28 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.connect(ADDRESS)
 
 server.send('''
-    Hello, world
-    Nice to meet you!
+oi
 ''')
 data = server.recv(SIZE)
 print data
+
+
+server.send('''
+tudo bem ?
+''')
+data = server.recv(SIZE)
+print data
+
+
+server.send('''
+tudo otimo e vc ?
+''')
+data = server.recv(SIZE)
+print data
+
+server.send('''
+que bom =D
+''')
+data = server.recv(SIZE)
+print data
+
